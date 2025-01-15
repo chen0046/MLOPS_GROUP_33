50018dce74fcd757d01a6eaf5d0b183e8a429064
Chenxi <chen0046@hotmail.com>
2025-01-15 16:53:03 +0100

    test

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,6 +1,9 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+
 from tests import _PATH_DATA
 from final_project.data import load_data, accuracy
-import os
 import torch 
 
 #Test the data loading
